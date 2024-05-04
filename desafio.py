18d3d2644711e3e3f1fa29547e23cd241366070e
Felipe Sanches <62717147+fnsanches@users.noreply.github.com>
2024-05-04 15:13:48 -0300

    Update desafio.py para POO

--- desafio.py
+++ desafio.py
@@ -1,63 +1,68 @@
-MENU = """
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+class ContaBancaria:
+    def __init__(self):
+        self.saldo = 0
+        self.limite = 500
+        self.extrato = ""
+        self.numero_saques = 0
+        self.LIMITE_SAQUES = 3
 
-=> """
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            self.extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação falhou! O valor informado é inválido.")
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    def sacar(self, valor):
+        excedeu_saldo = valor > self.saldo
+        excedeu_limite = valor > self.limite
+        excedeu_saques = self.numero_saques >= self.LIMITE_SAQUES
 
-def depositar():
-    global saldo, extrato
-    valor = float(input("Informe o valor do depósito: "))
-    if valor > 0:
-        saldo += valor
-        extrato += f"Depósito: R$ {valor:.2f}\n"
-    else:
-        print("Operação falhou! O valor informado é inválido.")
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+        elif valor > 0:
+            self.saldo -= valor
+            self.extrato += f"Saque: R$ {valor:.2f}\n"
+            self.numero_saques += 1
+        else:
+            print("Operação falhou! O valor informado é inválido.")
 
-def sacar():
-    global saldo, extrato, numero_saques
-    valor = float(input("Informe o valor do saque: "))
-    if valor <= 0:
-        print("Operação falhou! O valor informado é inválido.")
-        return
-    if valor > saldo:
-        print("Operação falhou! Você não tem saldo suficiente.")
-    elif valor > limite:
-        print("Operação falhou! O valor do saque excede o limite.")
-    elif numero_saques >= LIMITE_SAQUES:
-        print("Operação falhou! Número máximo de saques excedido.")
-    else:
-        saldo -= valor
-        extrato += f"Saque: R$ {valor:.2f}\n"
-        numero_saques += 1
+    def extrato_conta(self):
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not self.extrato else self.extrato)
+        print(f"\nSaldo: R$ {self.saldo:.2f}")
+        print("==========================================")
 
-def mostrar_extrato():
-    global saldo, extrato
-    print("\n================ EXTRATO ================")
-    print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"\nSaldo: R$ {saldo:.2f}")
-    print("==========================================")
 
 def main():
+    conta = ContaBancaria()
     while True:
-        opcao = input(MENU)
+        opcao = input(
+            """
+            [d] Depositar
+            [s] Sacar
+            [e] Extrato
+            [q] Sair
+
+            => """
+        )
         if opcao == "d":
-            depositar()
+            valor = float(input("Informe o valor do depósito: "))
+            conta.depositar(valor)
         elif opcao == "s":
-            sacar()
+            valor = float(input("Informe o valor do saque: "))
+            conta.sacar(valor)
         elif opcao == "e":
-            mostrar_extrato()
+            conta.extrato_conta()
         elif opcao == "q":
             break
         else:
             print("Operação inválida, por favor selecione novamente a operação desejada.")
+
 
 if __name__ == "__main__":
     main()
